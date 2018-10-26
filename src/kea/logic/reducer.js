18df9b4d413b4c523848babf7082cc5cacb244a3
Dan Lasky <dlasky@mediamath.com>
2018-10-26 15:11:14 -0400

    add support for object (keyed) reducer delcaration syntax

--- src/kea/logic/reducer.js
+++ src/kea/logic/reducer.js
@@ -1,74 +1,94 @@
-import { combineReducers } from 'redux'
+import { combineReducers } from "redux";
 
-function warnIfUndefinedActionCreator (object, property) {
-  if (process.env.NODE_ENV !== 'production') {
+function warnIfUndefinedActionCreator(object, property) {
+  if (process.env.NODE_ENV !== "production") {
     if (object.reducer.undefined !== undefined) {
-      console.warn(`A reducer with the property "${property}" is waiting for an action where its key is not defined.`)
+      console.warn(
+        `A reducer with the property "${property}" is waiting for an action where its key is not defined.`
+      );
     }
   }
 
-  return object
+  return object;
+}
+
+function warnIfMissingReducerObjectkeys(object, key) {
+  if (process.env.NODE_ENV !== "production") {
+    if (!object.value) {
+      console.warn(`Reducer "${key}" is missing an initial state value`);
+    }
+    if (!object.reducer) {
+      console.warn(`Reducer "${key}" is missing a reducer function`);
+    }
+  }
 }
 
 // create reducer function from such an object { [action]: (state, payload) => state }
-export function createReducer (mapping, defaultValue) {
+export function createReducer(mapping, defaultValue) {
   return (state = defaultValue, action) => {
     if (mapping[action.type]) {
-      return mapping[action.type](state, action.payload, action.meta)
+      return mapping[action.type](state, action.payload, action.meta);
     } else {
-      return state
+      return state;
     }
-  }
+  };
 }
 
-const emptyObj = {}
+const emptyObj = {};
 
 // input: object with values: { value, type, reducer, ...options } or function(state, action) {}
 // output: combined reducer function (state, action) {}
-export function combineReducerObjects (path, objects) {
-  const reducers = {}
+export function combineReducerObjects(path, objects) {
+  const reducers = {};
 
   Object.keys(objects).forEach(key => {
-    reducers[key] = objects[key].reducer
-  })
+    reducers[key] = objects[key].reducer;
+  });
 
   if (Object.keys(reducers).length > 0) {
-    return combineReducers(reducers)
+    return combineReducers(reducers);
   } else {
-    return () => emptyObj
+    return () => emptyObj;
   }
 }
 
 // input: object with values: [value, (type), (options), reducer]
 // output: object with values: { value, type, reducer, ...options }
-export function convertReducerArrays (reducers) {
+export function convertReducerArrays(reducers) {
   if (!reducers) {
-    return reducers
+    return reducers;
   }
 
-  const keys = Object.keys(reducers)
+  const keys = Object.keys(reducers);
   for (let i = 0; i < keys.length; i++) {
-    const s = reducers[keys[i]]
+    const s = reducers[keys[i]];
     if (Array.isArray(s)) {
       // s = [ value, (type), (options), reducer ]
-      const value = s[0]
-      const reducer = s[s.length - 1]
-      const type = typeof s[1] === 'function' ? s[1] : undefined
-      const options = typeof s[s.length - 2] === 'object' ? s[s.length - 2] : undefined
+      const value = s[0];
+      const reducer = s[s.length - 1];
+      const type = typeof s[1] === "function" ? s[1] : undefined;
+      const options =
+        typeof s[s.length - 2] === "object" ? s[s.length - 2] : undefined;
 
       let reducerObject = {
         value: value,
         type: type,
-        reducer: typeof reducer === 'function' ? reducer : createReducer(reducer, value)
-      }
+        reducer:
+          typeof reducer === "function"
+            ? reducer
+            : createReducer(reducer, value)
+      };
 
       if (options) {
-        reducerObject.options = options
+        reducerObject.options = options;
       }
 
-      reducers[keys[i]] = warnIfUndefinedActionCreator(reducerObject, keys[i])
+      reducers[keys[i]] = warnIfUndefinedActionCreator(reducerObject, keys[i]);
+    } else if (Object.isObject(s)) {
+      warnIfMissingReducerObjectkeys(s, keys[i]);
+      reducers[keys[i]] = warnIfUndefinedActionCreator(s, keys[i]);
     }
   }
 
-  return reducers
+  return reducers;
 }
